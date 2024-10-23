4fb76afb508240a543a7da7d70b10c6fd1cbdf74
karimi <karimi.wahidullah22@gmail.com>
2024-10-23 14:15:17 +0430

    some changes to styles

--- src/ImageCard.jsx
+++ src/ImageCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const ImageCard = ({ data }) => {
     return (
-        <div className='w-full flex justify-evenly items-center flex-wrap-reverse my-5'>
+        <div className='w-full flex justify-evenly items-center flex-wrap gap-3 my-5'>
             {data.map((curImage, index) => (
                 <div className='w-[200px] h-[400px]' key={index}>
                     <img src={curImage.download_url} alt="image" className='h-full w-full object-cover rounded-md hover:scale-110 hover:rotate-1' />
